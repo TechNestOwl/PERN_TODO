3ed748ec9cc127e347b35452577d573df461f14e
Gill Guimaraes <63368519+TechNestOwl@users.noreply.github.com>
2022-02-04 15:11:27 -0500

    delete todo added

--- server/app.js
+++ server/app.js
@@ -11,10 +11,6 @@ app.use(cors());
 app.use(express.json());//req.body
 // app.use(express.urlencoded());
 
-// app.get('/', (req,res) => {
-//     res.send("hello")
-// });
-
 
 //CREATE
 
@@ -57,9 +53,32 @@ app.get("/todos/:id", async (req, res) => {
 
 //UPDATE TODO
 
+app.put("/todos/:id", async (req,res) => {
+    try{
+        const {id} = req.params;
+        const {description} = req.body;
+        const updateTodo = await pool.query(
+            "UPDATE todo SET description =$1 WHERE todo_id = $2",
+            [description, id]
+            )
+            res.json("Todo updated");
+    }catch(err){
+        console.error(err.message)
+    }
+});
 
 //DELETE TOTO
 
+app.delete("/todos/:id", async (req,res) => {
+    try {
+        const {id} = req.params;
+        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1",[id]);
+
+        res.json("Todo deleted");
+    } catch (err) {
+        console.error(err.message)
+    }
+});
 
 
 app.listen(port, () =>{
